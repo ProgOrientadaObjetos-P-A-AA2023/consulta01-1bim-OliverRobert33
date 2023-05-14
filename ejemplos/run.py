812ba4c43de793bd51b44db4425c455a300372e4
Oliver <rosaraguro@utpl.edu.ec>
2023-05-13 19:06:16 -0500

    Practica Consulta

--- ejemplos/run.py
+++ ejemplos/run.py
@@ -2,17 +2,11 @@
 
 """
 # Crear dos objetos de la clase 01
-
 # objeto 01
-
 # crear
-
 # Presentar objeto; usar el método __st__
-
 # objeto 02
-
 # crear ingresando valores por teclado
-
 # Presentar objeto; usar el método __st__
 
 from mis_clases import Sueldo
